c09257150ff87862f5ca01968930f5f1625699e7
Gaultier Pierre <pit.gaultier@free.fr>
2019-03-27 14:38:14 +0100

    Update correct.js

--- js/correct.js
+++ js/correct.js
@@ -1,19 +1,9 @@
 var textNode;
 const walk = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT,null,false);
-const rExp = new RegExp('Emmanuel Macron|'+
-	'EmmanuelMacron|'+
-	'M[\.r] le Président de la République|'+ // Keep the dot in case of "Mr" or "M."
-	'le Président de la République|'+ // Put this one after, so that the one above matches first
-	'Monsieur le Président de la République|'+
-	'Monsieur le Président|'+
-	'Président de la République française|'+
-	'Président de la République|' +
-	'Le Président Macron|' +
-	'Emmanuel Jean-Michel Frédéric Macron|' +
-	'M[\.r] Macron', 'gi');
+const rExp = new RegExp('Toi|'+	'toi|', 'gi');
 
 while(textNode=walk.nextNode()) {
-    textNode.nodeValue = textNode.nodeValue.replace(rExp, 'Manu');
+    textNode.nodeValue = textNode.nodeValue.replace(rExp, 'Tuan');
 }
 
-document.title = document.title.replace(rExp, 'Manu');
+document.title = document.title.replace(rExp, 'Tuan');
